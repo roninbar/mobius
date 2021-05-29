41d0aedcd57ec6f2cfe08b2c23df9b65a22946b8
ron <rinbar70@gmail.com>
2021-05-29 16:09:28 +0300

    Replace the logo with a canvas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useEffect, useRef } from 'react'
 import './App.scss'
 
 function App() {
+  const canvas = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const gl = canvas.current?.getContext('webgl');
+    if (!gl) {
+      return;
+    }
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <canvas width="320px" height="320px" ref={canvas} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
