6b69ba0eb940d14b7ac60bd3b799a5c1aab22179
ron <rinbar70@gmail.com>
2021-05-30 22:05:05 +0300

    Updates

---
+++ precache-manifest.e02c8710d4dcf9db5cee8f7e9e15a2c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa478b29868029c4c1cd1d078a222f9",
+    "url": "/moebius-web/index.html"
+  },
+  {
+    "revision": "77893da859604d0fc98b",
+    "url": "/moebius-web/static/css/main.974df22a.chunk.css"
+  },
+  {
+    "revision": "01fadb5d82b4ab1a6c97",
+    "url": "/moebius-web/static/js/2.2d2ca185.chunk.js"
+  },
+  {
+    "revision": "0b285f52204d775f07fee2d388be9b90",
+    "url": "/moebius-web/static/js/2.2d2ca185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77893da859604d0fc98b",
+    "url": "/moebius-web/static/js/main.b91b62f1.chunk.js"
+  },
+  {
+    "revision": "8fed3ac8e8fa7f9803f3",
+    "url": "/moebius-web/static/js/runtime-main.448d6b11.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moebius-web/precache-manifest.da0f9570408c8d7f207c96adb8129a07.js"
+  "/moebius-web/precache-manifest.e02c8710d4dcf9db5cee8f7e9e15a2c3.js"
 );
 
 self.addEventListener('message', (event) => {
