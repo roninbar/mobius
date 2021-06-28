d613f69c7ebc3ffe63b60762c6fb1d1f6428bb6a
ron <rinbar70@gmail.com>
2021-06-28 12:46:36 +0300

    Updates

--- precache-manifest.b1b4431c5d42daf2e62b0369dc960b7e.js
+++ precache-manifest.b1b4431c5d42daf2e62b0369dc960b7e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5bce8afedf2fdaf451b37b6da399e7e",
+    "revision": "36541744876a03ab4dd942c239d99688",
     "url": "/mobius/index.html"
   },
   {
